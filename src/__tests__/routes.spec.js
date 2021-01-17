9b63ac241ef2a81b5445a43db5b1036c36862f21
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-16 22:53:58 -0700

    Adds script to run tests, adds base GET test
    Needs a relatively verbose script to run tests since we're running this app using modules and jest has't fully implemented that yet

---
+++ src/__tests__/routes.spec.js
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../server.js'
+
+describe('GET /', () => {
+  it('Returns status 200 healthy message', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err
+        done()
+      })
+  })
+})
