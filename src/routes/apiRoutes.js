417d733985a5e396ec82fc3f78a855c7e704e436
Bryan Yunis <bryany@booj.com>
2021-01-17 15:20:00 -0700

    Add route for getting city data
    This route will service the frontend by providing city results for a given search. That way the user can select the most relevant option at that point.

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import weatherRoutes from './weatherRoutes.js'
+import cityRoutes from './cityRoutes.js'
 
 const router = express.Router()
 
 router.use('/weather', weatherRoutes)
+router.use('/city', cityRoutes)
 
 export default router
