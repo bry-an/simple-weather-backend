bf817b37800fa883917d244db7ddd68b964d2afe
Bryan Yunis <bryany@booj.com>
2021-01-16 17:44:03 -0700

    Create base GET health-check route with 200 status

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,11 @@ import apiRoutes from './apiRoutes.js'
 
 const router = express.Router()
 
-router.use('/api', apiRoutes)
-
-router.use('*', function (req, res) {
-  res.set('Content-Type', 'text/html').send(
-    new Buffer.from(`
-<h2>404</h2>
-`)
-  )
+router.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'Healthy'
+  })
 })
+router.use('/api', apiRoutes)
 
 export default router
